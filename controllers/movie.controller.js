55743774460169b3d5dcf34b5a9db6aaab80334e
Giorgi Gagishvili <g.gagishvili@noxtton.com>
2024-07-27 21:57:27 +0400

    feat: get movies + query

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,11 +1,25 @@
 const Movie = require('../models/movie.model.js')
 
-const getMovies = async (_, res) => {
-    const movies = await Movie.find({});
-    res.status(200).json({
-        success: true,
-        data: movies,
-    })
+const getMovies = async (req, res) => {
+    try {
+        const { name } = req.query
+        let query = {}
+        if (name) {
+            query.name = { $regex: name, $options: 'i' };
+        }
+        const movies = await Movie.find(query)
+
+        res.status(200).json({
+            success: true,
+            data: movies,
+        })
+    } catch (err) {
+        ///ToDo replace
+        res.status(500).json({
+            success: false,
+            error: err
+        })
+    }
 }
 
 module.exports = {
